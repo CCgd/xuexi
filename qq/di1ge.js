4b5d4ec960e9ca9ba94c838c8114b63b01ed6b1c
CCgd <chenchang.gd@gmail.com>
2017-06-11 14:39:29 +0800

    Create di1ge.js

---
+++ qq/di1ge.js
@@ -0,0 +1,15 @@
+var http = require('http')
+
+var server = http.createServer()
+
+server.on('request',function(req,res){
+
+    var url = req.url
+    if 
+})
+
+
+
+server.listen(3000,function(){
+    console.log('听着呢大哥')
+})
